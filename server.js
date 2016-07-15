4bb1287792b46c8387f56d5423b43ecfd233918d
Harin Sanghirun <harin.sanghirun@gmail.com>
2016-07-16 01:14:34 +0700

    use strict

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
-var express = require('express');
-var app = express();
+"use strict";
+let express = require('express');
+let app = express();
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
