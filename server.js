60948b73ea205d147ec0a343746224bec6524005
Harin Sanghirun <harin.sanghirun@gmail.com>
2016-07-16 01:22:12 +0700

    express static assets

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 "use strict";
 let express = require('express');
+let path = require('path');
 let app = express();
 
+app.use('/assets', express.static('build'))
+
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-let port = process.env.PORT || 8080;
+let port = process.env.PORT || 3000;
 
 app.listen(port, function () {
-  console.log('Example app listening on port 3000!');
+  console.log('Example app listening on port ' + port + '!');
 });
